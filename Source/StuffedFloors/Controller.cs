4404461d726467b46906d9c3e250a7de8bbb6d20
FluffierThanThou <fluffy.l2032@gmail.com>
2016-12-25 18:54:39 +0100

    Working implied generation plus hookup to ArchitectSense

---
+++ Source/StuffedFloors/Controller.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using ArchitectSense;
+using HugsLib;
+using RimWorld;
+using Verse;
+using static StuffedFloors.ImpliedDefGenerator;
+
+namespace StuffedFloors
+{
+    public class Controller : ModBase
+    {
+        public override string ModIdentifier => "StuffedFloors";
+
+        public override void Initialize()
+        {
+            // get all the floortypedefs from the game's library
+            var floorTypes = DefDatabase<FloorTypeDef>.AllDefsListForReading;
+
+            // for each type, generate a set of terrainDefs
+            floorTypes.ForEach( GenerateTerrainDefs );
+        }
+
+        public void GenerateTerrainDefs( FloorTypeDef floorType )
+        {
+            // for each allowed stuff, generate a TerrainDef
+            var terrainDefs = GetStuffDefsFor( floorType.stuffCategories ).Select( floorType.GetStuffedTerrainDef ).ToList();
+            
+            // note that this happens _after_ defs are loaded and implied defs are generated, so we still need to do that manually.
+            GenerateAndStoreImpliedThingDefs( terrainDefs );
+            
+            // add terrainDefs to the defDatabase
+            DefDatabase<TerrainDef>.Add( terrainDefs );
+            
+            // from this list of defs, generate a SubCategoryDef
+            CreateArchitectSubCategory( floorType, terrainDefs );
+        }
+
+        private static void CreateArchitectSubCategory( FloorTypeDef floorType, List<TerrainDef> terrainDefs )
+        { 
+            DesignationSubCategoryDef subCategoryDef = new DesignationSubCategoryDef();
+            subCategoryDef.label = floorType.label;
+            subCategoryDef.designationCategory = floorType.designationCategory;
+            subCategoryDef.preview = false; // we want the stuff-like selector
+
+            // poke ArchitectSense
+            ArchitectSense.Controller.AddSubCategory( DefDatabase<DesignationCategoryDef>.GetNamed( "Floors" ), subCategoryDef, terrainDefs.ToList() );
+        }
+
+        private Dictionary<StuffCategoryDef, List<ThingDef>> _stuffCache = new Dictionary<StuffCategoryDef, List<ThingDef>>();
+
+        public List<ThingDef> GetStuffDefsFor( List<StuffCategoryDef> stuffCategoryDefs )
+        {
+            return stuffCategoryDefs.SelectMany( GetStuffDefsFor ).ToList();
+        }
+
+        public List<ThingDef> GetStuffDefsFor( StuffCategoryDef stuffCategory )
+        {
+            // there doesn't seem to be a good way to get a list of things by stuffType, so we'll have to match the list manually.
+            // I really don't want to loop this list for every single floorTypeDef, so let's cache this
+            List<ThingDef> stuffs;
+
+            // try get from cache
+            if ( _stuffCache.TryGetValue( stuffCategory, out stuffs ) )
+                return stuffs;
+
+            // manually fetch list.
+            stuffs = DefDatabase<ThingDef>.AllDefsListForReading
+                                          .Where( td => td.IsStuff && td.stuffProps.categories.Contains( stuffCategory ) )
+                                          .ToList();
+
+            // cache
+            _stuffCache.Add( stuffCategory, stuffs );
+            return stuffs;
+        }
+    }
+}
---
+++ Source/StuffedFloors/FloorTypeDef.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace StuffedFloors
+{
+    public class FloorTypeDef : TerrainDef
+    {
+        // TerrainDef basically contains all the fields we need, so just derive from that.
+        // note that we're hardwiring stuffed terrains to the Floors category, that way we
+        // can leave designationCategory on null for the floorType, and we don't have to deal
+        // with the game adding the category placeholder itself as a buildable floor. This is 
+        // not exactly a perfect solution, and should be refactored at some point in the (near)
+        // future.a
+        public int stuffCost;
+
+        public override IEnumerable<string> ConfigErrors()
+        {
+            List<string> errors = base.ConfigErrors()?.ToList() ?? new List<string>();
+            if (designationCategory != null)
+                errors.Add( "FloorTypeDef should not have designationCategory set (hardcoded to Floors)" );
+
+            return errors;
+        }
+
+        public TerrainDef GetStuffedTerrainDef( ThingDef stuffThingDef )
+        {
+            if ( !stuffThingDef.IsStuff )
+                throw new ArgumentException( stuffThingDef.defName + " is not a stuff!" );
+            
+            // create new terrain
+            TerrainDef terrain = new TerrainDef
+            {
+                acceptFilth = acceptFilth,
+                acceptTerrainSourceFilth = acceptTerrainSourceFilth,
+                affordances = affordances.NullOrEmpty() 
+                                ? new List<TerrainAffordance>() 
+                                : new List<TerrainAffordance>( affordances ),
+                avoidWander = avoidWander,
+                altitudeLayer = altitudeLayer,
+                changeable = changeable,
+                driesTo = driesTo,
+                designationCategory = DefDatabase<DesignationCategoryDef>.GetNamed( "Floors" ),
+                edgeType = edgeType,
+                fertility = fertility,
+                holdSnow = holdSnow,
+                layerable = layerable,
+                menuHidden = menuHidden,
+                passability = passability,
+                pathCost = pathCost,
+                pathCostIgnoreRepeat = pathCostIgnoreRepeat,
+                placeWorkers = placeWorkers.NullOrEmpty() 
+                                ? new List<Type>() 
+                                : new List<Type>( placeWorkers ),
+                placingDraggableDimensions = placingDraggableDimensions,
+                renderPrecedence = renderPrecedence,
+                repairEffect = repairEffect,
+                researchPrerequisites = researchPrerequisites.NullOrEmpty() 
+                                            ? new List<ResearchProjectDef>() 
+                                            : new List<ResearchProjectDef>( researchPrerequisites ),
+                scatterType = scatterType,
+                smoothedTerrain = smoothedTerrain,
+                specialDisplayRadius = specialDisplayRadius,
+                statBases = statBases.NullOrEmpty() ? new List<StatModifier>() : new List<StatModifier>( statBases ),
+                texturePath = texturePath,
+                takeFootprints = takeFootprints,
+                terrainFilthDef = terrainFilthDef,
+                terrainAffordanceNeeded = terrainAffordanceNeeded
+            };
+            
+            // apply stuff elements
+            StuffProperties stuff = stuffThingDef.stuffProps;
+            terrain.color = stuff.color;
+            terrain.label = "ThingMadeOfStuffLabel".Translate( stuffThingDef.LabelAsStuff, label );
+            terrain.description = description;
+            terrain.defName = stuffThingDef.defName + "_" + defName;
+            terrain.costList = new List<ThingCountClass>();
+            if (!costList.NullOrEmpty())
+                foreach ( ThingCountClass cost in costList )
+                    terrain.costList.Add( new ThingCountClass( cost.thingDef, cost.count ) );
+            if (stuffCost > 0)
+                terrain.costList.Add( new ThingCountClass( stuffThingDef, (int)( stuffCost / stuffThingDef.VolumePerUnit ) ) );
+
+            // TODO: Implement stuff stat offsets
+
+            // we need to assign hashes
+            terrain.GiveShortHash();
+
+            // done!
+            terrain.PostLoad();
+            return terrain;
+        }
+    }
+}
