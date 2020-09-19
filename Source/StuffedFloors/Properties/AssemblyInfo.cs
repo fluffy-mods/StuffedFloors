db627ab6aaff99b72591b298fb7f58381e85b1b3
Fluffy <fluffy.l2032@gmail.com>
2020-09-19 21:52:17 +0200

    update xml defs

--- Source/StuffedFloors/Properties/AssemblyInfo.cs
+++ Source/StuffedFloors/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.9.82")]
+[assembly: AssemblyFileVersion("2.11.89")]
